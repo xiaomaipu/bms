03b09ad6ae7a9d735fe5bd6ab5db6959043b8605
小麦铺 <38550411+xiaomaipu@users.noreply.github.com>
2018-05-09 13:58:02 +0800

    Update app.js
    修改全局配置

--- src/main/resources/webapp/library/src/app.js
+++ src/main/resources/webapp/library/src/app.js
@@ -7,20 +7,14 @@ App({
 	globalData: {
 
 
-		baseUrl: 'https://bms.24xiaomai.com',
-		// baseUrl: 'http://47.94.52.121:8064',
-
-		imageUrl: 'http://static.24xiaomai.com/go/wxapplet/1.3.0/image',
+		baseUrl: '小程序服务器地址',
+		imageUrl: '静态资源地址',
 		header: {
 			'Cookie': '', //公用请求头
 			'content-type': 'application/json'
 		},
 		common: {
-			isFirst: true,
-			shopCode: '', //门店信息
-			shopName: '', //门店名字
-			souce: '', //来源 1 不开门 0 开门
-			selfPay: null, //付款页跳转至『我的』页面时，订单发生了变化，要重新获取订单信息，wx.switchTab: url 不支持 queryString，使用见pay.js
+			
 		},
 		userInfo: {
 			openId: "", //唯一标示
